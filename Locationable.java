461a2cb5fd7e5c216a9d846a68cd2a47c16b9f6c
Daris02 <hei.raymond.2@gmail.com>
2025-05-13 12:40:43 +0300

    init main class

---
+++ Locationable.java
@@ -0,0 +1,12 @@
+public class Locationable {
+  private String name;
+  private String description;
+  private boolean isLocaled;
+
+  public Locationable(String name, String description) {
+    this.name = name;
+    this.description = description;
+    this.isLocaled = false;
+  }
+  
+}
