4bd49dcb85e4cb9718c1dcfb68c374adea3a4e3f
Daris02 <hei.raymond.2@gmail.com>
2025-05-14 09:25:03 +0300

    feat: add timeout

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const promises_1 = require("readline/promises");
 const process_1 = require("process");
 const config_1 = require("./config/config");
 exports.readline = (0, promises_1.createInterface)({ input: process_1.stdin, output: process_1.stdout });
+const TIMEOUT_DURATION = 10000;
+async function askWithTimeout(question) {
+    return Promise.race([
+        exports.readline.question(question),
+        new Promise((resolve) => setTimeout(() => {
+            resolve(null);
+            (0, config_1.logError)("\nTimeout! Please try again.");
+            exports.readline.close();
+            process.exit(1);
+        }, TIMEOUT_DURATION)),
+    ]);
+}
 async function main() {
     (0, console_1.log)("\n----------------------------");
     (0, config_1.logInfo)("Welcome to Locationable");
@@ -16,34 +28,34 @@ async function main() {
     (0, console_1.log)("3. Make Location.");
     (0, console_1.log)("4. Cancel reservation.");
     (0, console_1.log)("5. Exit.");
-    const userInput = await exports.readline.question("Please enter your choice: ");
+    const userInput = await askWithTimeout("Please enter your choice: ");
     switch (userInput) {
         case "1":
             (0, Location_1.showAllThingsWithState)();
-            let choice = await exports.readline.question("Do you want reserve? (y/n): ");
+            let choice = await askWithTimeout("Do you want reserve? (y/n): ");
             if (choice.toLowerCase() === "y") {
-                let name = await exports.readline.question("Enter the name of the thing: ");
+                let name = await askWithTimeout("Enter the name of the thing: ");
                 (0, Location_1.makeLocation)(name);
             }
             main();
             break;
         case "2":
-            let thingName = await exports.readline.question("Enter the name of the thing (REQUIRED): ");
-            let description = await exports.readline.question("Enter the description of the thing: ");
+            let thingName = await askWithTimeout("Enter the name of the thing (REQUIRED): ");
+            let description = await askWithTimeout("Enter the description of the thing: ");
             if (!(0, Location_1.addThings)(thingName, description)) {
-                (0, config_1.logWarning)('Please retry again!!!');
+                (0, config_1.logWarning)("Please retry again!!!");
             }
             main();
             break;
         case "3":
-            let reserveName = await exports.readline.question("Enter name of thing you want to reserved: ");
+            let reserveName = await askWithTimeout("Enter name of thing you want to reserved: ");
             (0, Location_1.makeLocation)(reserveName);
             main();
             break;
         case "4":
             if ((0, Location_1.showAllThingsReserved)() == 0)
                 main();
-            let cancelName = await exports.readline.question("Enter name of thing you want to cancel: ");
+            let cancelName = await askWithTimeout("Enter name of thing you want to cancel: ");
             (0, Location_1.cancelReservation)(cancelName);
             main();
             break;
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,84 @@
 import { log } from "console";
-import { addThings, cancelReservation, makeLocation, showAllThingsReserved, showAllThingsWithState } from "./Location";
+import {
+  addThings,
+  cancelReservation,
+  makeLocation,
+  showAllThingsReserved,
+  showAllThingsWithState,
+} from "./Location";
 import { createInterface } from "readline/promises";
 import { stdin as input, stdout as output } from "process";
-import { logInfo, logWarning } from "./config/config";
+import { logError, logInfo, logWarning } from "./config/config";
 
 export const readline = createInterface({ input, output });
+const TIMEOUT_DURATION = 10_000;
+
+async function askWithTimeout(question: string): Promise<string | null> {
+  return Promise.race([
+    readline.question(question),
+    new Promise<string | null>((resolve) =>
+      setTimeout(() => {
+        resolve(null);
+        logError("\nTimeout! Please try again.");
+        readline.close();
+        process.exit(1);
+      }, TIMEOUT_DURATION)
+    ),
+  ]);
+}
 
 async function main() {
   log("\n----------------------------");
   logInfo("Welcome to Locationable");
   log("----------------------------");
-  log("1. All Things.")
-  log("2. Add Things.")
-  log("3. Make Location.")
-  log("4. Cancel reservation.")
-  log("5. Exit.")
-  const userInput = await readline.question("Please enter your choice: ");
+  log("1. All Things.");
+  log("2. Add Things.");
+  log("3. Make Location.");
+  log("4. Cancel reservation.");
+  log("5. Exit.");
+  const userInput = await askWithTimeout("Please enter your choice: ");
 
   switch (userInput) {
     case "1":
       showAllThingsWithState();
-      let choice = await readline.question("Do you want reserve? (y/n): ");
+      let choice = await askWithTimeout("Do you want reserve? (y/n): ");
       if (choice.toLowerCase() === "y") {
-        let name = await readline.question("Enter the name of the thing: ");
+        let name = await askWithTimeout("Enter the name of the thing: ");
         makeLocation(name);
       }
       main();
       break;
 
     case "2":
-      let thingName = await readline.question("Enter the name of the thing (REQUIRED): ");
-      let description = await readline.question("Enter the description of the thing: ");
+      let thingName = await askWithTimeout(
+        "Enter the name of the thing (REQUIRED): "
+      );
+      let description = await askWithTimeout(
+        "Enter the description of the thing: "
+      );
       if (!addThings(thingName, description)) {
-        logWarning('Please retry again!!!')
+        logWarning("Please retry again!!!");
       }
-      main()
+      main();
       break;
 
     case "3":
-      let reserveName = await readline.question("Enter name of thing you want to reserved: ");
+      let reserveName = await askWithTimeout(
+        "Enter name of thing you want to reserved: "
+      );
       makeLocation(reserveName);
       main();
       break;
 
     case "4":
       if (showAllThingsReserved() == 0) main();
-      let cancelName = await readline.question("Enter name of thing you want to cancel: ");
+      let cancelName = await askWithTimeout(
+        "Enter name of thing you want to cancel: "
+      );
       cancelReservation(cancelName);
       main();
       break;
-    
+
     case "5":
       log("Goodbye!");
       readline.close();
